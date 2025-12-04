5d136ac49f0d6b78eebffa168d0646cfaab476f1
tzuyuwu21 <joywu1221@gmail.com>
2025-12-04 17:58:08 +0800

    更新天氣API請求中的地點從宜蘭縣改為桃園市

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /**
- * 取得高雄天氣預報
+ * 取得桃園天氣預報
  * CWA 氣象資料開放平臺 API
  * 使用「一般天氣預報-今明 36 小時天氣預報」資料集
  */
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "宜蘭縣",
+          locationName: "桃園市",
         },
       }
     );
